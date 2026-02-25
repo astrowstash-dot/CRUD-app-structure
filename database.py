2655fa733327fcb26e8654ec6ce9e686fdc94a02
Rahuldev <Rahul.Ramchandra@outlook.com>
2026-02-25 23:55:03 +0530

    Configure SQLAlchemy engine, session, and Base

--- database.py
+++ database.py
@@ -4,3 +4,10 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}
+)
+
+Sessionlocal = sessionmaker(bind = engine, autoflush = False, autocommit = False)
+
+Base = declarative_base()
